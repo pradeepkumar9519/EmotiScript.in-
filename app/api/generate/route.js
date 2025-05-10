8fb4fc561c222439e14d297d3d77844865906ae1
pradeepkumar9519 <pm656975@gmail.com>
2025-05-10 22:23:04 +0530

    route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,12 +1,10 @@
-// app/api/generate/route.js
-
 export async function POST(req) {
   const { prompt } = await req.json();
 
-  const GEMINI_API_KEY = "AIzaSyBxo1EBv3S7hC2FkToEfPrINMn-27Vx3Ek"; // Aapka real API key
+  const GEMINI_API_KEY = "AIzaSyBxo1EBv3S7hC2FkToEfPrINMn-27Vx3Ek";
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`,
     {
       method: 'POST',
       headers: {
